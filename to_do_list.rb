b8245aced61e31fbb5d828923593cee7e2ea5918
codeamac02 <codeamac02@gmail.com>
2015-06-11 09:22:49 -0500

    App skeleton

---
+++ to_do_list.rb
@@ -0,0 +1,33 @@
+class List
+	def initialize
+		
+	end
+
+	def show_list
+		
+	end
+	
+end
+
+class Task
+	def initialize(name, date)
+		@name = name
+		@date = date
+	end
+
+	def edit_task
+		
+	end
+
+	def delete_task
+		
+	end
+
+	def show_task
+		
+	end
+
+	def complete?
+		
+	end
+end
